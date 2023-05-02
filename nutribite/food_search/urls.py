516eb819cc3cfe65ae18cd132077a5229ec8b190
DevkarPrasad <devkarprasad00@gmail.com>
2023-05-02 23:45:49 +0530

    corrected

--- nutribite/food_search/urls.py
+++ nutribite/food_search/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 
     path('', views.food_search, name='food_search'),
-    path('food/', views.nutrients_view, name='nutrients'),
+ #path('food/', views.nutrients_view, name='nutrients'),
 
 ]
